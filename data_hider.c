765ab771a45f0cf743408529890c57df4e4ac893
Dean <dgramcko@csus.edu>
2020-11-27 12:14:14 -0800

    Fixed bug in datahider so that it can handle multiple occurrences of FFD9 trailer

--- data_hider.c
+++ data_hider.c
@@ -16,7 +16,7 @@ int hide_chunk(char* path_to_jpeg, char* chunk, size_t chunk_size)
 	int len = st.st_size;
     //create a buffer to hold the jpg + the new data
 
-    size_t size_of_result = (len + chunk_size) * sizeof(unsigned char);
+    size_t size_of_result = (len + chunk_size + 3) * sizeof(unsigned char); // adding 3 bytes to length so I can include my own trailer after the jpeg trailer
  	unsigned char *data_to_write = (unsigned char*) malloc(size_of_result);
 		
 	//read jpg into buffer 
@@ -25,25 +25,34 @@ int hide_chunk(char* path_to_jpeg, char* chunk, size_t chunk_size)
 	
 	char tmpBuffer[512];
     int j = 0;
-    int end_marker = -1;
-    int done = 0;
-    while (fread(tmpBuffer, 1, 512, jpg) && (!done))
+    int end_marker = -1; //the offset of the first free byte of memory after the final FFD9 (and the added  0x24 0xFF 0xD9 trailer we put in)
+
+    //copy all of the data from the jpeg into the buffer
+    while (fread(tmpBuffer, 1, 512, jpg))
     {
         for (int i = 0; i < 512; i++)
         {
             data_to_write[j] = tmpBuffer[i];
-            if(data_to_write[j-1] == 0xff && data_to_write[j] == 0xd9) // stop copying when we find the jpeg trailer
+            if(j > 0)
             {
-                done=1;
-                j++;
-                break;
+                if(data_to_write[j-1] == 0xff && data_to_write[j] == 0xd9) //  make note every time we find FFD9 
+                {
+                    end_marker = j; // store as end_marker until we find the last one
+                }
             }
-            j++; //  j now is the array offset to the next empty byte
+            j++; 
         }
     }
-    size_of_result=(j+chunk_size) * sizeof(unsigned char);
+    //after all the data has been copied into the buffer, figure out where the final FFD9 was and start writing data there.
 
-    fclose(jpg);
+    size_of_result=( (end_marker+1) + 3 + chunk_size) * sizeof(unsigned char); // size of result = <bytes of data from jpeg> + <3 bytes for our trailer> + <size of data to hide>
+
+    //write our custom trailer in so the extractor knows where the data begins
+    data_to_write[end_marker+1]=0x24;
+    data_to_write[end_marker+2]=0xFF;
+    data_to_write[end_marker+3]=0xD9;
+    j = end_marker+4;
+
     //copy the new data into the data_to_write buffer 
     for(int k = 0; k < chunk_size; k++)
     {
@@ -89,7 +98,7 @@ unsigned char* extract_chunk(char* path_to_jpeg, int* size)
         for (int i = 0; i < 512; i++)
         {
             jpg[j] = tmpBuffer[i];
-            if(jpg[j-1] == 0xff && jpg[j] == 0xd9) // found the jpeg trailer
+            if(jpg[j-4] == 0xff &&jpg[j-3] == 0xd9 &&jpg[j-2] == 0x24 && jpg[j-1] == 0xff && jpg[j] == 0xd9) // found the jpeg trailer
             {
                 end_of_jpg=j;
             }
@@ -111,10 +120,11 @@ unsigned char* extract_chunk(char* path_to_jpeg, int* size)
 int main()
 {
     
-    char *test_jpg = "/media/Enclave/school/sac_state/Courses/2020/Fall/CSC153/CSC-153---Final-Project/chapel_mountain_sky_alps.jpg";
+    char *test_jpg = "/media/Enclave/school/sac_state/Courses/2020/Fall/CSC153/CSC-153---Final-Project/IMG_1713.JPG";
 
-    char test_chunk[14]= "this is a test";
-    hide_chunk(test_jpg, test_chunk, 14);
+    char test_chunk[17]= "This is a test123";
+    
+    hide_chunk(test_jpg, test_chunk, 17);
 
     int size_of_chunk;
     int *size = &size_of_chunk;
